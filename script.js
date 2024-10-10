43d208ce30c9da5a1d316a19447cfce5135a7b32
NNUGRAHH <jalanlebar45@gmail.com>
2024-10-10 11:53:53 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+// Timer
+let timerElement = document.getElementById('timer');
+let time = 3600; // 1 hour in seconds
+
+let timerInterval = setInterval(() => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    timerElement.innerHTML = `${minutes}:${seconds}`;
+    time--;
+
+    if (time < 0) {
+        clearInterval(timerInterval);
+        alert('Waktu habis!');
+    }
+}, 1000);
+
+// Navigasi Soal (masih dummy, bisa diimprove)
+function nextSoal() {
+    alert('Soal berikutnya belum ada.');
+}
